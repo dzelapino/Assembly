a90b91de84de89fe09e1cd297e9d19292c85ea23
kkolodziejski <kolodziejskikrzysztofhubert@gmail.com>
2022-08-10 13:19:58 +0200

    Quick ver.2

--- Nasm/quickloop.asm
+++ Nasm/quickloop.asm
@@ -1,23 +1,25 @@
 %include "io64.inc"
 
+section .data
+counter db 0
+
 section .text
 
 global CMAIN
 CMAIN:
     mov rbp, rsp; for correct debugging  
-    mov rax, 10
-    sub rax, 2
-    jnz notzero
-    mov rcx, 30
-    jz sucess
-                      
+    mov rax, 35
+    jmp notzero
+    
 notzero:
-    sub rax, 2
+    add byte[counter], 1
+    sub rax, 1
     jnz notzero
     mov rdx, 30
     jz sucess
 
 sucess:
-    mov rbx, 30
+    sub rbx, 29
     xor rax, rax
     ret
+    
